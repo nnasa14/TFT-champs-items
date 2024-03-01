c7d5602c5b2157ed452b8a4f1c1d5349ad6d4ff3
nnasa14 <nnascimento.kr@gmail.com>
2024-03-01 16:09:40 -0500

    secured API key & attempt to access API

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const champion = "https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/tft-champion.json";
+require('dotenv').config();
 
-/* fetch(champion, {
-    headers: {
-        'Authorization': `Bearer ${apiKey}`,
-        'Content-Type': 'application/json'
-    }
-}) */
-axios.get(champion)
-    .then(response => {
-        console.log(response.data)
-    })
-    .catch(error => {
-        console.error(error.response.data)
-    })
+document.addEventListning("DOMContentLoaded", function () {
+    const apiKey = process.env.API-KEY;
+    const championUrl = "https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/tft-champion.json";
+    const champContainer = document.getElementById('champ');
+    const button = document.getElementById('button');
+
+    let getChampion = () => {
+        fetch(championUrl, {
+            headers: {
+                'Authorization': `Bearer ${apiKey}`,
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                const champName = data.champ[0].name;
+                champContainer.textContext = `${champName}`;
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+            });
+
+    button.addEventListning("click", getChampion);
+    getChampion();
+    });
