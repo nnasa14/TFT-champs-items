4f55ecfb30159004adb5b466f30ef11b45bf70e4
nnasa14 <nnascimento.kr@gmail.com>
2024-03-01 21:25:32 -0500

    get items occurs three times

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 /* API urls */
 const champUrl = 'https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/tft-champion.json';
 const traitUrl = 'https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/tft-trait.json';
+const itemUrl = 'https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/tft-item.json';
+const itemIdUrl = 'https://ddragon.leagueoflegends.com/cdn/14.4.1/data/en_US/item.json';
 
 /* HTML containers */
 const traitContainer = document.getElementById('trait');
 const champContainer = document.getElementById('champ');
+const itemsContainer = document.getElementById('items-container');
 
 const button = document.getElementById('button');
+
+/* remove spaces and special chars */
+let justChars = name => {
+    const regex = /[^a-zA-Z]/g;
+
+    return name.replace(regex, '');
+}
 
 let getChamp = () => {
     fetch(champUrl)
@@ -15,8 +25,23 @@ let getChamp = () => {
             const champNames = Object.values(data.data);
             const randomIndex = Math.floor(Math.random() *champNames.length);
             const champ = champNames[randomIndex];
-            const image = `https://ddragon.leagueoflegends.com/cdn/14.4.1/img/champion/${champ.name}.png`;
-            champContainer.innerHTML = `${champ.name} <br> <img src="${image}">`;
+            const imageName = justChars(champ.name);
+            const image = `https://ddragon.leagueoflegends.com/cdn/14.4.1/img/champion/${imageName}.png`;
+            champContainer.innerHTML = `${champ.name} <img src="${image}">`;
+        })
+        .catch(error => console.error('Fetch error:', error));
+}
+
+let getItem = () => {
+    fetch(itemUrl)
+        .then(response => response.json())
+        .then(data => {
+            const items = Object.values(data.data);
+            const randomIndex = Math.floor(Math.random() *items.length);
+            const item = items[randomIndex]; 
+            const imageName = justChars(item.name);
+            const image = `https://cdn.mobalytics.gg/assets/tft/images/game-items/set10/${imageName}.png?v=52`
+            itemsContainer.innerHTML += `${item.name} <br> <img src="${image}">`;
         })
         .catch(error => console.error('Fetch error:', error));
 }
@@ -36,6 +61,10 @@ let getTrait = () => {
 button.addEventListener("click", function () {
     getChamp();
     getTrait();
-});
+    itemsContainer.innerHTML = '';
+    for (let i = 0; i < 3; i++) {
+        getItem();}
+    });
 getChamp();
 getTrait();
+getItem();
